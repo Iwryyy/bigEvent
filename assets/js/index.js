c5586c85a4f83b15f32c2196984518dcf45d2899
Iwryyy <l1uzc@foxmail.com>
2021-11-20 16:16:33 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,50 +15,52 @@ $(function() {
     })
 
 
-    function getUserInfo() {
-        $.ajax({
-            method: 'GET',
-            url: '/my/userinfo',
-            // headers: {
-            //     Authorization: localStorage.getItem('token') || ''
-            // },
-            success: function(res) {
-                if (res.status !== 0) {
-                    return layui.layer.msg('获取用户信息失败')
-                }
-                console.log(res);
-                // 调用renderAvatar渲染
-                renderAvatar(res.data)
-            },
-            // 无论成功还是失败都调用这个函数
-            // complete: function(res) {
-            //     console.log(res);
-            //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-            //         // 强制清空token
-            //         localStorage.removeItem('token')
-            //             // 强制跳转
-            //         location.href = '/login.html'
-            //     }
-            // }
-        })
-    }
 
-    // 渲染用户的头像
-    function renderAvatar(user) {
-        // 获取用户名称
-        var name = user.nickname || user.username
-            // 设置欢迎的文本
-        $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
-            // 按需渲染用户的头像
-        if (user.user_pic !== null) {
-            $('.layui-nav-img').attr('src', user.user_pic).show()
-            $('.text-avater').hide()
-        } else {
-            // 按文本渲染
-            $('.layui-nav-img').hide()
-            var first = name[0].toUpperCase()
-            $('.text-avatar').html(first).show()
-        }
-
-    }
 })
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            // console.log(res);
+            // 调用renderAvatar渲染
+            renderAvatar(res.data)
+        },
+        // 无论成功还是失败都调用这个函数
+        // complete: function(res) {
+        //     console.log(res);
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         // 强制清空token
+        //         localStorage.removeItem('token')
+        //             // 强制跳转
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+}
+
+// 渲染用户的头像
+function renderAvatar(user) {
+    // 获取用户名称
+    var name = user.nickname || user.username
+        // 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        // 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        // 按文本渲染
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+
+}
